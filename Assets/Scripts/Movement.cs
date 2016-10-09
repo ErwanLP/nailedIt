d6e6393bdc32d4a0ba626b52ebc79f332011639d
NLEPODER <nicolas.lepoder@gmail.com>
2016-10-09 22:28:09 +0200

    script input

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement  {
+
+    private Vector3 initPosition;
+    private Vector3 finalPosition;
+    private float initTime;
+    private float finalTime;
+
+    public Movement()
+    {
+    }
+
+    public Movement(Vector3 initPosition)
+    {
+        this.initPosition = initPosition;
+    }
+
+    public Movement(Vector3 initPosition, Vector3 finalPosition)
+    {
+        this.initPosition = initPosition;
+        this.finalPosition = finalPosition;
+    }
+
+
+    public void setInitPosition(Vector3 initPosition)
+    {
+        this.initPosition = initPosition;
+        this.initTime = Time.time;
+    }
+
+    public void setFinalPosition(Vector3 finalPosition)
+    {
+        this.finalPosition = finalPosition;
+        this.finalTime = Time.time;
+    }
+
+    public void log()
+    {
+        Debug.Log("Spped Y : " + this.getYSpped());
+    }
+
+    public float getDeltaTime()
+    {
+        return this.finalTime - this.initTime;
+    }
+    public Vector3 getDeltaPosition()
+    {
+        return this.initPosition - this.finalPosition;
+    }
+    public float getYSpped()
+    {
+        return this.getDeltaPosition().y / this.getDeltaTime();
+    }
+
+
+
+}
---
+++ Assets/Scripts/inputManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class inputManager : MonoBehaviour {
+
+    public Movement m;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Handle native touch events
+        foreach (Touch touch in Input.touches)
+        {
+            HandleTouch(touch.fingerId, Camera.main.ScreenToWorldPoint(touch.position), touch.phase);
+        }
+
+        // Simulate touch events from mouse events
+        if (Input.touchCount == 0)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                HandleTouch(10, Input.mousePosition, TouchPhase.Began);
+            }
+            if (Input.GetMouseButton(0))
+            {
+                HandleTouch(10, Input.mousePosition, TouchPhase.Moved);
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                HandleTouch(10, Input.mousePosition, TouchPhase.Ended);
+            }
+        }
+    }
+
+    private void HandleTouch(int touchFingerId, Vector3 touchPosition, TouchPhase touchPhase)
+    {
+        switch (touchPhase)
+        {
+            case TouchPhase.Began:
+                this.m = new Movement(touchPosition);
+                break;
+            case TouchPhase.Ended:
+                this.m.setFinalPosition(touchPosition);
+                this.m.log();
+                break;
+        }
+    }
+}
