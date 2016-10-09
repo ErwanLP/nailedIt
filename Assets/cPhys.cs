f29cbc8728e353f004740efa06a36ee3c7109a44
NLEPODER <nicolas.lepoder@gmail.com>
2016-10-09 21:34:31 +0200

    set up unity

---
+++ Assets/cPhys.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cPhys : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.drag = 30;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
