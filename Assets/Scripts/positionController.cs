d00e35407ada78eb667745744d2ed1ba10566b87
NLEPODER <nicolas.lepoder@gmail.com>
2016-10-30 22:03:39 +0100

    Osef^2

--- Assets/Scripts/positionController.cs
+++ Assets/Scripts/positionController.cs
@@ -4,25 +4,19 @@ using System.Collections;
 
 public class positionController : MonoBehaviour {
 
-    public GameObject posClouGO;
-    public GameObject posTableGO;
-    public Text tEcart;
-    public Text tWin;
+    public GameObject clou;
     private bool win = false;
 
     // Use this for initialization
     void Start () {
-	
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	    if (posClouGO.transform.position.y <= posTableGO.transform.position.y)
+	    if (clou.transform.position.y <= 0)
         {
             win = true;
+            Debug.Log(win);
         }
-
-        tEcart.text = ("Ecart : "+ (posClouGO.transform.position.y - posTableGO.transform.position.y));
-        tWin.text = ("Win : " + win);
     }
 }
