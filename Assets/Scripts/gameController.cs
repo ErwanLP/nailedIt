c1facbb7f739ea66cdc6c52c6cbc631fc23f0540
Nicolas LE PODER <Nicolas LE PODER>
2016-12-21 15:43:06 +0100

    Add Player.cs

--- Assets/Scripts/gameController.cs
+++ Assets/Scripts/gameController.cs
@@ -3,20 +3,40 @@ using System.Collections;
 
 public class gameController : MonoBehaviour {
 
-    public int numberPlayer = 1;
-    public Hammer hammer;
+    public int numberPlayer;
+    public Player[] playerList;
     public InputManager im;
+    public int selectedPlayer;
 
 
 	// Use this for initialization
 	void Start () {
-            this.hammer =  new Hammer();
-            this.im = new InputManager(handleInputManager);
+
+        this.numberPlayer = 2;
+        Debug.Log("Nomber Player : " + this.numberPlayer);
+
+        // create player array;
+        playerList = new Player[this.numberPlayer];
+        for (int i = 0; i < numberPlayer; i++)
+        {
+            Debug.Log("CREATION GAMER : " + i);
+            playerList[i] = new Player();
+            playerList[i].getHammer().getGameObject().SetActive(false);
+        }
+        this.selectedPlayer = 0;
+        playerList[this.selectedPlayer].getHammer().getGameObject().SetActive(true);
+
+        this.im = new InputManager(handleInputManager);
     }
 
     private void handleInputManager(Movement m)
     {
-        this.hammer.getGameObject().GetComponent<mPhys>().boom(m);
+
+        playerList[this.selectedPlayer].getHammer().getGameObject().GetComponent<mPhys>().boom(m);
+        StartCoroutine(this.Example());
+        //playerList[this.selectedPlayer].getHammer().getGameObject().SetActive(false);
+        this.incrementeSelectedGamer();
+        //playerList[this.selectedPlayer].getHammer().getGameObject().SetActive(true);
     }
 
     // Update is called once per frame
@@ -24,5 +44,25 @@ public class gameController : MonoBehaviour {
         this.im.getInput();
 
     }
+
+    private void incrementeSelectedGamer()
+    {
+        this.selectedPlayer++;
+        if(this.selectedPlayer == numberPlayer)
+        {
+            this.selectedPlayer = 0;
+        }
+    }
+
+    private IEnumerator Example()
+    {
+        print(Time.time);
+        yield return new WaitForSeconds(5);
+        print(Time.time);
+    }
+
+
+
+
 
 }
---
+++ assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    private Hammer hammer;
+
+    public Player()
+    {
+        this.hammer = new Hammer();
+    }
+
+    public Hammer getHammer()
+    {
+        return this.hammer;
+    }
+
+}
