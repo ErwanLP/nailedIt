b6f6a8a2715c89ed7104d21e27666339962452a2
NLEPODER <nicolas.lepoder@gmail.com>
2017-05-28 17:01:21 +0200

    Yolo

---
+++ Assets/Scripts/loadScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadScene : MonoBehaviour {
+
+	// Use this for initialization
+	public void chooseGameModeSolo () {
+        PlayerPrefs.SetInt("GameMode", 1);
+        changeLevel();
+    }
+
+    public void chooseGameModeMulti()
+    {
+        PlayerPrefs.SetInt("GameMode", 2);
+        changeLevel();
+    }
+
+    void changeLevel()
+    {
+        SceneManager.LoadScene("sGame");
+    }
+
+
+}
