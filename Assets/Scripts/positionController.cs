ce854a832b56b2af294b640aab66b1e46f2635dd
NLEPODER <nicolas.lepoder@gmail.com>
2017-05-28 17:00:46 +0200

    Yolo

--- Assets/Scripts/positionController.cs
+++ Assets/Scripts/positionController.cs
@@ -9,8 +9,10 @@ public class positionController : MonoBehaviour {
     public BoxCollider bc_table;
     public BoxCollider bc_clou;
     public Text t_delta;
+    public Text t_gameMode;
     private bool win = false;
     private float delta;
+    private string gameMode;
 
     // Use this for initialization
     void Start () {
@@ -21,6 +23,18 @@ public class positionController : MonoBehaviour {
         delta = (bc_clou.bounds.center.y + bc_clou.bounds.size.y / 2) - (bc_table.bounds.center.y + bc_table.bounds.size.y / 2);
 
         t_delta.text = "Delta : " +delta;
+
+        switch (PlayerPrefs.GetInt("GameMode"))
+        {
+            case 1:
+                gameMode = "Solo";
+                break;
+            case 2:
+                gameMode = "Multijoueur";
+                break;
+        }
+
+        t_gameMode.text = "Game Mode : " + gameMode;
 
         if (delta <= 0)
         {
